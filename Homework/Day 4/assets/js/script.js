28743e887d35af1436398a6ce959fcf9cc478e9a
kang0408 <148173085+kang0408@users.noreply.github.com>
2023-11-15 10:53:16 +0700

    create scroll to top button

--- Homework/Day 4/assets/js/script.js
+++ Homework/Day 4/assets/js/script.js
@@ -1,6 +1,6 @@
 let mySideNav = document.querySelector('.side-nav');
 let myExit = document.querySelector('.exit-bar');
-
+// When click button open bar, show exit bar and side nav
 function showSideNav() {
     if(mySideNav.style.left === "-100%" && myExit.style.display === "none") {
         mySideNav.style.left = "0";
@@ -9,9 +9,24 @@ function showSideNav() {
         mySideNav.style.left = "-100%";
         myExit.style.display = "none";
     }
-    
+    // When click content, undisplay exit bar and side nav
     if(mySideNav.style.left === "0" && myExit.style.display === "flex") {
         mySideNav.style.left = "-100%";
         myExit.style.display = "none";
     }
+}
+
+let myScroll = document.querySelector('.scroll');
+// When scroll down 20px from the top of the document, show the scroll
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myScroll.style.display = "flex";
+    } else {
+        myScroll.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; //For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
